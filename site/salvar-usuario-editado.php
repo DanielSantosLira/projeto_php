b1dbf8f6cf6951257fe77a3fc9bd211e75f6e781
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-06-12 09:03:10 -0300

    Update salvar-usuario-editado.php

--- site/salvar-usuario-editado.php
+++ site/salvar-usuario-editado.php
@@ -6,9 +6,9 @@ $senja = $_POST["senha"];
 
 include "conexao.php";
 
-$sql_iditar_jogo = "update usuario set nome= '$nome', email='$email',  senha='md5($senha)' where id = $id";
+$sql_iditar_usuario = "update usuario set nome= '$nome', email='$email',  senha='".md5($senha)."' where id = $id";
 
-$um_jogo = mysqli_query($conexao, $sql_iditar_jogo);
+$um_usuario = mysqli_query($conexao, $sql_iditar_usuario);
 
 header("location:listar-usuario.php?msg=sucesso");
 ?>
